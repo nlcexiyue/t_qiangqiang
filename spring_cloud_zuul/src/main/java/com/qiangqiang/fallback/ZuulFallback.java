f471bbf699143084e2caeb3dff6cd2da03b0c837
nlcexiyue <nlcexiyue@126.com>
2021-01-11 23:48:36 +0800

    服务容错之请求合并 服务容错之服务隔离 服务容错之服务熔断 服务容错之服务降级 Feign雪崩处理

---
+++ spring_cloud_zuul/src/main/java/com/qiangqiang/fallback/ZuulFallback.java
@@ -0,0 +1,68 @@
+package com.qiangqiang.fallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class ZuulFallback implements FallbackProvider {
+
+    /**
+     * 返回fallback处理哪个服务，返回的是服务名称
+     * @return
+     */
+    @Override
+    public String getRoute() {
+        return "product-server";
+    }
+
+    /**
+     * 服务容错处理
+     * @param route 容错服务名称
+     * @param cause 服务异常信息
+     * @return
+     */
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 500;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "error";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("The service is unavailable.".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
---
+++ spring_cloud_zuul/src/main/java/com/qiangqiang/filter/CustomFilter.java
@@ -0,0 +1,58 @@
+package com.qiangqiang.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Component
+public class CustomFilter extends ZuulFilter {
+
+    /**
+     * 过滤器类型
+     * pre
+     * routing
+     * post
+     * error
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 执行顺序，数值越小，优先级越高
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 执行条件 true开启  false关闭
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 动作，具体逻辑
+     * @return
+     * @throws ZuulException
+     */
+    @Override
+    public Object run() throws ZuulException {
+        //是zuul包下的RequestContext
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        System.out.println(request.getMethod() + "---"+ request.getRequestURI().toString());
+        return null;
+    }
+}
